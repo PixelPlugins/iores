8ca7e0d6dfd4ec3f94d9b6fe63312cf48b92d566
PixelPlugins <omgami@mail.com>
2017-12-26 19:48:34 -0600

    Add files via upload

---
+++ plug.js
@@ -0,0 +1,33 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+	
+	ext.httpreq = function(url){
+		var retval = 'Err';
+		xhr = new XMLHttpRequestRequest();
+		xhr.onreadystatechange = function(){
+			if(this.readyState == 4 && this.status == 200){
+				retval = this.responseText;
+			}
+		}
+		xhr.open('GET', url, false);
+		xhr.send();
+		return retval;
+	}
+	
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+			['r', 'Send HTTP Request to %s', 'httpreq', 'http://www.google.com']
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Sample extension', descriptor, ext);
+})({});
